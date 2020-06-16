0a291228f71ad22c797dd377929957bba41d5fb4
Nacht <realnacht@outlook.com>
2020-06-16 21:25:21 +0800

    feat: define color theme

---
+++ src/utils/theme.js
@@ -0,0 +1,18 @@
+const dark = "#000";
+const white = "#fff";
+
+export const lightTheme = {
+  colors: {
+    primary: "#33e",
+    background: white,
+    body: dark,
+  },
+};
+
+export const DarkTheme = {
+  colors: {
+    primary: "#a0c",
+    background: dark,
+    body: white,
+  },
+};
