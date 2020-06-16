b152a8c8a638aee2ca8a504674578aaccfe7d89c
Nacht <realnacht@outlook.com>
2020-06-17 01:54:21 +0800

    feat: warp root with theme provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,10 @@
 // custom typefaces
 import "typeface-montserrat";
 import "typeface-merriweather";
-
+import React from "react";
+import { ThemeProvider } from "./src/context";
 import "prismjs/themes/prism-tomorrow.css";
+
+export const wrapRootElement = ({ element }) => {
+  return <ThemeProvider>{element}</ThemeProvider>;
+};
